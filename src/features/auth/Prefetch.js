3e0d161006a7d3c98365d831cdda4e10bebf8bc7
swapnesh86 <swapnesh.j@gmail.com>
2023-03-01 01:13:56 +0800

    Changes to Prefetch

--- src/features/auth/Prefetch.js
+++ src/features/auth/Prefetch.js
@@ -7,17 +7,17 @@ import { Outlet } from 'react-router-dom';
 const Prefetch = () => {
     useEffect(() => {
         //console.log('subscribing')
-        const skus = store.dispatch(skusApiSlice.endpoints.getSkus.initiate())
-        const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate())
+        /* const skus = store.dispatch(skusApiSlice.endpoints.getSkus.initiate())
+        const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate()) */
 
-        /* store.dispatch(skusApiSlice.util.prefetch('getSkus', 'skuList', { force: true }))
-        store.dispatch(usersApiSlice.util.prefetch('getUsers', 'usersList', { force: true })) */
+        store.dispatch(skusApiSlice.util.prefetch('getSkus', 'skuList', { force: true }))
+        store.dispatch(usersApiSlice.util.prefetch('getUsers', 'usersList', { force: true }))
 
-        return () => {
+        /* return () => {
             //console.log('unsubscribing')
             skus.unsubscribe()
             users.unsubscribe()
-        }
+        } */
     }, [])
 
     return <Outlet />
